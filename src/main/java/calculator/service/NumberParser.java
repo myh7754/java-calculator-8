331b64de0755a7ae5da3c7df9d11b9c001736f92
myh <myh4755@gmail.com>
2025-10-18 02:02:16 +0900

    docs(README): 예외처리 수정

--- src/main/java/calculator/service/NumberParser.java
+++ src/main/java/calculator/service/NumberParser.java
@@ -5,7 +5,11 @@ import java.util.Collection;
 public class NumberParser {
 
     public Collection<?> parse(String message) {
+        if (message == null || message.isEmpty()) {
+            throw new IllegalArgumentException("message is null or empty");
+        }
 
-        return
+
+        return null;
     }
 }
