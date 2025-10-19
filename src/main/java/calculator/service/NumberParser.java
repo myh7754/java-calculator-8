5bfb0187e9dd23f18bdc20dfc3fcd2cf29483db9
myh <myh4755@gmail.com>
2025-10-20 03:05:32 +0900

    fix(service): 정규 표현식 오류 수정 기존 정수만 체크하던 정규식을 소수도 포함하여 체크할 수 있도록 변경

--- src/main/java/calculator/service/NumberParser.java
+++ src/main/java/calculator/service/NumberParser.java
@@ -13,7 +13,7 @@ public class NumberParser {
 
     private static final String DEFAULT_DELIMITER = ",|:"; // 기본 정규식
     private static final Pattern CUSTOM_DELIMITER = Pattern.compile("//(.)\\\\n(.*)"); // 커스텀 정규식
-    private static final Pattern VALID_NUMBER_PATTERN = Pattern.compile("^[0-9]+$");// 숫자만 (시작~끝까지)
+    private static final Pattern VALID_NUMBER_PATTERN = Pattern.compile("^[0-9]+(\\.[0-9]+)?$");// 숫자만 (시작~끝까지)
 
     public Collection<Double> parse(String message) {
         if (message == null || message.isEmpty()) {
