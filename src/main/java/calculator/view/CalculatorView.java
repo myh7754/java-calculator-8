1762b73a92b780142513b53af5946606db5422ca
myh <myh4755@gmail.com>
2025-10-20 23:20:54 +0900

    refactor(view): 상수 메시지 upper_snake_case로 수정

--- src/main/java/calculator/view/CalculatorView.java
+++ src/main/java/calculator/view/CalculatorView.java
@@ -1,15 +1,15 @@
 package calculator.view;
 
 public class CalculatorView {
-    public static final String introMessage = "덧셈할 문자열을 입력해 주세요.";
-    public static final String resultMessage = "결과 : ";
+    public static final String INTRO_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    public static final String RESULT_MESSAGE = "결과 : ";
 
     public void display() {
-        System.out.println(introMessage);
+        System.out.println(INTRO_MESSAGE);
     }
 
     public void printMessage(Number result) {
-        System.out.println(resultMessage + result);
+        System.out.println(RESULT_MESSAGE + result);
     }
 
 
