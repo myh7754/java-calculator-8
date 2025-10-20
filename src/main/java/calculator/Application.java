0b1d514255699a98fbbfcc1ebcd1f77376aaaddb
myh <myh4755@gmail.com>
2025-10-20 23:21:50 +0900

    refactor : 기타 필요없는 주석 제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,7 +9,6 @@ import calculator.view.CalculatorView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         CalculatorView view = new CalculatorView();
         NumberParser numberParser = new NumberParser();
         CalculatorService service = new CalculatorServiceImpl(numberParser);
